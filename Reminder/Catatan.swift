95777303888f6523d56a33b3976dc9af874fca13
Gita Venesia Setiadi <gitavns@akramamrullahs-MacBook-Pro.local>
2016-05-14 22:53:16 +0700

    Initial commit

---
+++ Reminder/Catatan.swift
@@ -0,0 +1,16 @@
+//
+//  Catatan.swift
+//  Reminder
+//
+//  Created by Gita Venesia Setiadi on 5/14/16.
+//  Copyright © 2016 gitavns. All rights reserved.
+//
+
+import Foundation
+
+class Catatan {
+    var nama: String
+    var deskripsi: String
+    
+    
+}
